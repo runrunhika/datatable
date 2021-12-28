bdb272007d6fa9a84346c9f6e6d6bf41f68e8de7
runrunhika <71953944+runrunhika@users.noreply.github.com>
2021-12-29 05:30:01 +0900

    first commit

---
+++ lib/data/sort_table/users.dart
@@ -0,0 +1,27 @@
+import 'package:datatable_exmple/model/sort_table/user.dart';
+
+final allUsers = <User>[
+  User(firstName: 'A', lastName: '大', age: 37),
+  User(firstName: 'B', lastName: '大', age: 27),
+  User(firstName: 'C', lastName: '大', age: 20),
+  User(firstName: 'a', lastName: '小', age: 37),
+  User(firstName: 'b', lastName: '小', age: 27),
+  User(firstName: 'c', lastName: '小', age: 20),
+  User(firstName: 'や', lastName: '大', age: 37),
+  User(firstName: 'ゆ', lastName: '大', age: 27),
+  User(firstName: 'ゃ', lastName: '小', age: 21),
+  User(firstName: 'ゅ', lastName: '小', age: 24),
+  User(firstName: 'AA', lastName: '大', age: 42),
+  User(firstName: 'aa', lastName: '小', age: 47),
+  User(firstName: '1', lastName: '小', age: 18),
+  User(firstName: '2', lastName: '小', age: 33),
+  User(firstName: '3', lastName: '小', age: 19),
+  User(firstName: '１', lastName: '大', age: 18),
+  User(firstName: '２', lastName: '大', age: 33),
+  User(firstName: '３', lastName: '大', age: 19),
+  User(firstName: '一', lastName: '漢字', age: 18),
+  User(firstName: '二', lastName: '漢字', age: 33),
+  User(firstName: '三', lastName: '漢字', age: 19),
+  User(firstName: '伊', lastName: '漢字', age: 19),
+  User(firstName: '意', lastName: '漢字', age: 19),
+];
